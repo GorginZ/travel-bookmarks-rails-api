08564b3825f88f5debdeac2756a235d4c4b98695
GorginZ <georgia.z.leng@gmail.com>
2020-06-30 14:55:51 +1000

    changed controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,37 @@
 class BookmarksController < ApplicationController
-    def index 
-        @bookmarks = Bookmark.all
-        render json: @bookmarks
-      end 
+  before_action :set_bookmark, only: [:show, :update, :destroy]
 
-      def create
-       Bookmark.create(bookmark_params)
-       render json: "created bookmark", status: :created
-      end
+  def index 
+    bookmarks = Bookmark.all.order(id: "asc")
+    render json: bookmarks
+  end 
 
-      private
-      def bookmark_params
-        params.require(:bookmark).permit(:title, :url, :description)
-      end
+  def show 
+    render json: @bookmark
+  end 
+
+  def create 
+    Bookmark.create(bookmark_params)
+    render json: "created bookmark", status: :created
+  end 
+
+  def update 
+    @bookmark.update(bookmark_params)
+    render json: "bookmark updated", status: 200
+  end 
+
+  def destroy
+    @bookmark.destroy
+    render json: "bookmark deleted", status: 200
+  end 
+
+  private 
+
+  def bookmark_params 
+    params.require(:bookmark).permit(:title, :description, :url)
+  end 
+
+  def set_bookmark 
+    @bookmark = Bookmark.find(params[:id])
+  end 
 end
